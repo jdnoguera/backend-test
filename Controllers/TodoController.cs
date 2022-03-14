1e6c7c1fe406a1c8397c1c65736a3b086e603cad
jdnoguera <josias17.noguera@gmail.com>
2022-03-14 15:03:14 -0300

    initial commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend_test.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TodoController : ControllerBase
+{
+    private static List<Todo> todoList = new List<Todo>
+    {
+        new Todo
+        {
+            Id = 1,
+            Description = "Prueba"
+        }
+    };
+    
+    [HttpGet]
+    public async Task<ActionResult<List<Todo>>> Get()
+    {
+        return Ok(todoList);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<List<Todo>>> AddTodo(Todo todo)
+    {
+        todoList.Add(todo);
+        return Ok(todoList);
+    }
+    
+ }
---
+++ Todo.cs
@@ -0,0 +1,7 @@
+﻿namespace backend_test;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+}
